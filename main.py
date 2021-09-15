ae03db523da124d6d821f14adc57c71d2bac7deb
qdaniel4 <su0978hi@go.minnstate.edu>
2021-09-15 16:18:34 -0500

    added a delete book function (delete_book) and delete book menu item (7)

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """ Program to create and manage a list of books that the user wishes to read, and books that the user has read. """
 
-from bookstore import Book, BookStore
+from bookstore import Book, BookStore, BookError
 from menu import Menu
 import ui
 
@@ -26,6 +26,7 @@ def create_menu():
     menu.add_option('4', 'Show Read Books', show_read_books)
     menu.add_option('5', 'Show All Books', show_all_books)
     menu.add_option('6', 'Change Book Read Status', change_read)
+    menu.add_option('7', 'Delete Book', delete_book)
     menu.add_option('Q', 'Quit', quit_program)
 
     return menu
@@ -64,6 +65,17 @@ def change_read():
     new_read = ui.get_read_value()     
     book.read = new_read 
     book.save()
+
+
+def delete_book():
+    book_id = ui.get_book_id()
+
+    book_deleted = store.get_book_by_id(book_id)
+    if book_deleted is None:
+        print('Error: Book Not Found')
+    else:
+        book_deleted.delete()
+
     
 
 def quit_program():
